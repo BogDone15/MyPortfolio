54cba8982ce85959e236ced509716ff0bc556173
Bogdan <buselb2@gmail.com>
2020-11-09 21:18:37 +0200

    add functionality canvas, create works section

--- js/app.js
+++ js/app.js
@@ -20,58 +20,100 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		})
 	});
 
-		let canvas = document.getElementById('canvas');
+	const canvas = document.getElementById('canvas');
 
-		if (canvas) {
-			let c      = canvas.getContext('2d');
+	if (canvas) {
+		const c = canvas.getContext('2d');
 
-		c.fillStyle = 'black';
 		c.strokeStyle = '#e3e3e3';
-	//Declare vars t:time, i:eased function of time, steps:num of steps per revolution
-		let t      = 0;
-		let i      = 0;
-		let steps  = 200;
-		function refresh(){
-			if(t%steps === 0 && t!=0){
+		//Declare vars t:time, i:eased function of time, steps:num of steps per revolution
+		let t = 0;
+		let i = 0;
+		let steps = 200;
+		function refresh() {
+			if (t % steps === 0 && t != 0) {
 				t = 0;
 			}
 			let x = 0;
 			let y = 0;
 
-	    //easing function on time using cosine wave
-			let i = -(Math.cos(Math.PI/steps*t)+1)/2;
+			let n = 0;
+			let m = 0;
 
-	    //get value between 0 and 2 Pi proportional to i
-			i *= Math.PI*2;
-	    //define x and y values for point using wavey waves
-			x = Math.sin(i)*100;
-			y = -Math.cos(i)*100;
+			let q = 0;
+			let w = 0;
 
-	    //centre the point
-			x += 250;
+			let e = 0;
+			let z = 0;
+
+			//easing function on time using cosine wave
+			let i = -(Math.cos(Math.PI / steps * t) + 1) / 2;
+
+			//get value between 0 and 2 Pi proportional to i
+			i *= Math.PI * 2;
+			//define values for point using wavey waves
+			x = Math.sin(i) * 100;
+			y = Math.cos(i) * 100;
+
+			n = -Math.sin(i) * 100;
+			m = -Math.cos(i) * 100;
+
+			q = Math.sin(-i) * 100;
+			w = Math.cos(-i) * 100;
+
+			e = -Math.sin(-i) * 100;
+			z = -Math.cos(-i) * 100;
+
+			//centre the point
+			x += 150;
 			y += 250;
 
-	    //clear current thing with a bit of a motion blur
-		   c.clearRect(0,0,500,500);
-	    c.fillStyle = 'rgba(25,25,25, 0.5)';
-	    // c.fillRect(0,0,500,500);
-	    c.fillStyle = 'white';
+			n += 150;
+			m += 250;
 
-	    //draw circle
+			q += 350;
+			w += 250;
+
+			e += 350;
+			z += 250;
+
+			//clear current thing with a bit of a motion blur
+			c.clearRect(0, 0, 500, 500);
+			c.fillStyle = 'white';
+
+			//draw circle
 			c.beginPath();
-			c.arc(250,250,100,0,Math.PI*2);
+
+			c.arc(150, 250, 100, 0, Math.PI * 2);
 			c.stroke();
 
-	    //draw point
 			c.beginPath();
-			c.arc(x,y,10,0,Math.PI*2);
+
+			c.arc(350, 250, 100, 0, Math.PI * 2);
+			c.stroke();
+
+			//draw point
+			c.beginPath();
+			c.arc(x, y, 8, 0, Math.PI * 2);
+			c.fill();
+
+			c.beginPath();
+			c.arc(n, m, 8, 0, Math.PI * 2);
+			c.fill();
+
+			c.beginPath();
+			c.arc(q, w, 8, 0, Math.PI * 2);
+			c.fill();
+
+			c.beginPath();
+			c.arc(e, z, 8, 0, Math.PI * 2);
 			c.fill();
 
 			t += 1;
 		}
 		window.setInterval(refresh, 20);
-		}
-		
+	}
+
 	if (canvasSkills) {
 		try {
 			TagCanvas.Start('canvas__skills', 'tags', {
